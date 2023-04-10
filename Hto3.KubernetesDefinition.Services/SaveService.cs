d67b256bab64ec0692b81976213e535e165952bd
Hélio Tibagí <helio_net@hotmail.com>
2023-04-09 21:14:13 -0300

    --Bug fix environment variable with a number as value.

--- Hto3.KubernetesDefinition.Services/SaveService.cs
+++ Hto3.KubernetesDefinition.Services/SaveService.cs
@@ -1,12 +1,15 @@
 ﻿using Hto3.CollectionHelpers;
 using Hto3.KubernetesDefinition.Models.Contracts;
+using Hto3.KubernetesDefinition.Models.KubernetesObjects;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YamlDotNet.Core;
 using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.EventEmitters;
 using YamlDotNet.Serialization.NamingConventions;
 
 namespace Hto3.KubernetesDefinition.Services
@@ -19,6 +22,7 @@ namespace Hto3.KubernetesDefinition.Services
         {
             this.serializer = new SerializerBuilder()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .WithEventEmitter(nextEmitter => new CustomizeEnvVarEventEmitter(nextEmitter))
                 .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitDefaults)
                 .Build();
         }
@@ -54,6 +58,39 @@ namespace Hto3.KubernetesDefinition.Services
                 serializer.Serialize(textWriter, item.KubernetesDataObject);
                 if (item != graph.Last())
                     textWriter.WriteLine("---");
+            }
+        }
+
+        private class CustomizeEnvVarEventEmitter : ChainedEventEmitter
+        {
+            private Boolean envVarEmitting = false;
+
+            public CustomizeEnvVarEventEmitter(IEventEmitter nextEmitter) : base(nextEmitter)
+            {
+            }
+
+            public override void Emit(ScalarEventInfo eventInfo, IEmitter emitter)
+            {
+                if (envVarEmitting && Int32.TryParse(eventInfo.Source.Value?.ToString() ?? String.Empty, out var number))
+                    eventInfo.Style = ScalarStyle.DoubleQuoted;
+
+                base.Emit(eventInfo, emitter);
+            }
+
+            public override void Emit(MappingStartEventInfo eventInfo, IEmitter emitter)
+            {
+                if (eventInfo.Source.StaticType == typeof(EnvVar))
+                    envVarEmitting = true;
+
+                base.Emit(eventInfo, emitter);
+            }
+
+            public override void Emit(MappingEndEventInfo eventInfo, IEmitter emitter)
+            {
+                if (eventInfo.Source.StaticType == typeof(EnvVar))
+                    envVarEmitting = false;
+
+                base.Emit(eventInfo, emitter);
             }
         }
     }
